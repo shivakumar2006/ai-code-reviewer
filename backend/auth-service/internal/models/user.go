25c9c5a1f39f07e61abdf9812c891a5dce151a69
shivakumar2006 <official.shivakumar06@gmail.com>
2026-07-13 23:16:17 +0530

    feat: add validation tags to RegisterRequest fields

--- backend/auth-service/internal/models/user.go
+++ backend/auth-service/internal/models/user.go
@@ -24,9 +24,9 @@ type RefreshToken struct {
 }
 
 type RegisterRequest struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Name     string `json:"name" validate:"required, min=2, max=50"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"password, min=8"`
 }
 
 type LoginRequest struct {
